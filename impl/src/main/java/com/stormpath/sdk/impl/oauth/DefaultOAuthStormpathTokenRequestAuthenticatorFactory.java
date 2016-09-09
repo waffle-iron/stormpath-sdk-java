3e65337cd2a6156d69d851788558738633762e43
Matt Raible <matt@raibledesigns.com>
2016-09-09 07:57:14 -0700

    Changed 'Social' to 'Token'

--- impl/src/main/java/com/stormpath/sdk/impl/oauth/DefaultOAuthStormpathTokenRequestAuthenticatorFactory.java
+++ impl/src/main/java/com/stormpath/sdk/impl/oauth/DefaultOAuthStormpathTokenRequestAuthenticatorFactory.java
@@ -2,15 +2,15 @@ package com.stormpath.sdk.impl.oauth;
 
 import com.stormpath.sdk.application.Application;
 import com.stormpath.sdk.impl.application.DefaultApplication;
-import com.stormpath.sdk.oauth.OAuthStormpathSocialGrantRequestAuthenticator;
-import com.stormpath.sdk.oauth.OAuthStormpathSocialRequestAuthenticatorFactory;
+import com.stormpath.sdk.oauth.OAuthStormpathTokenGrantRequestAuthenticator;
+import com.stormpath.sdk.oauth.OAuthStormpathTokenRequestAuthenticatorFactory;
 
 /**
  * @since 1.1.0
  */
-public class DefaultOAuthStormpathTokenRequestAuthenticatorFactory implements OAuthStormpathSocialRequestAuthenticatorFactory {
+public class DefaultOAuthStormpathTokenRequestAuthenticatorFactory implements OAuthStormpathTokenRequestAuthenticatorFactory {
     @Override
-    public OAuthStormpathSocialGrantRequestAuthenticator forApplication(Application application) {
-        return ((DefaultApplication) application).createStormpathSocialGrantAuthenticator();
+    public OAuthStormpathTokenGrantRequestAuthenticator forApplication(Application application) {
+        return ((DefaultApplication) application).createStormpathTokenGrantAuthenticator();
     }
 }
